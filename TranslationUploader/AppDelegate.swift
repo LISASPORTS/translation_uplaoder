1205c72d03ac9e9556833e1fdc4d2bf50cd1f368
Marcin Rudnicki <marcin.rudnicki@amsterdam-standard.pl>
2020-11-20 11:36:02 +0100

    Initial Commit

---
+++ TranslationUploader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TranslationUploader
+//
+//  Created by Marcin Rudnicki on 20/11/2020.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
